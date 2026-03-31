5bd99e2d86a0beb2adbc0bf960579cdcf8571b19
AhmedHuzaifa2001 <huzaifamahzoom2001@gmail.com>
2026-03-31 10:12:18 +0530

    Added all Repos of Ticket

---
+++ webapp/src/main/java/com/zenith/webapp/ticket/repository/TicketRepository.java
@@ -0,0 +1,17 @@
+package com.zenith.webapp.ticket.repository;
+
+import com.zenith.webapp.ticket.enums.TicketPriority;
+import com.zenith.webapp.ticket.enums.TicketStatus;
+import com.zenith.webapp.ticket.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findByStatus(TicketStatus status);
+    List<Ticket> findByPriority(TicketPriority priority);
+}
+
+
+// JpaRepository is a Spring Data JPA interface that provides ready‑made 
+// database operations.
