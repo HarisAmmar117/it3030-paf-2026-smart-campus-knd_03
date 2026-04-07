40ff4fcec3d74239dd32a428936d2adf408586c1
HarisAmmar117 <mhmammar19@gmail.ccom>
2026-04-07 12:13:07 +0530

    created booking model

---
+++ webapp/src/main/java/com/zenith/webapp/booking/model/Booking.java
@@ -0,0 +1,42 @@
+// package com.zenith.webapp.booking.model;
+
+// import java.time.LocalDateTime;
+
+// import org.hibernate.annotations.CreationTimestamp;
+
+// import com.zenith.webapp.facility.model.Resource;
+
+// import jakarta.persistence.*;
+
+// @Entity
+// @Table(name = "bookings")
+// public class Booking {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long booking_id;
+
+//     // Many bookings → One user
+//     @ManyToOne
+//     @JoinColumn(name = "user_id", nullable = false)
+//     private com.zenith.webapp.auth.model.User user;
+
+//     // Many bookings → One resource
+//     @ManyToOne
+//     @JoinColumn(name = "resource_id", nullable = false)
+//     private Resource resource;
+
+
+//     @CreationTimestamp
+//     private LocalDateTime startTime;
+//     private LocalDateTime endTime;
+
+//     private String purpose;
+//     private int attendees;
+
+//     @Enumerated(EnumType.STRING)
+//     private Status status = Status.PENDING;
+
+
+//     // getters & setters
+// }
