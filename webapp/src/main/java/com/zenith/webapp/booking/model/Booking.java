5281be592809c5d917fd16e8614fc1e8887cf4ff
HarisAmmar117 <mhmammar19@gmail.ccom>
2026-04-07 12:20:14 +0530

    created booking status enum

--- webapp/src/main/java/com/zenith/webapp/booking/model/Booking.java
+++ webapp/src/main/java/com/zenith/webapp/booking/model/Booking.java
@@ -1,42 +1,43 @@
-// package com.zenith.webapp.booking.model;
+package com.zenith.webapp.booking.model;
 
-// import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
-// import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.CreationTimestamp;
 
-// import com.zenith.webapp.facility.model.Resource;
+import com.zenith.webapp.booking.enums.BookingStatus;
+import com.zenith.webapp.facility.model.Resource;
 
-// import jakarta.persistence.*;
+import jakarta.persistence.*;
 
-// @Entity
-// @Table(name = "bookings")
-// public class Booking {
+@Entity
+@Table(name = "bookings")
+public class Booking {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long booking_id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long booking_id;
 
-//     // Many bookings → One user
-//     @ManyToOne
-//     @JoinColumn(name = "user_id", nullable = false)
-//     private com.zenith.webapp.auth.model.User user;
+    // Many bookings → One user
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private com.zenith.webapp.auth.model.User user;
 
-//     // Many bookings → One resource
-//     @ManyToOne
-//     @JoinColumn(name = "resource_id", nullable = false)
-//     private Resource resource;
-
-
-//     @CreationTimestamp
-//     private LocalDateTime startTime;
-//     private LocalDateTime endTime;
-
-//     private String purpose;
-//     private int attendees;
-
-//     @Enumerated(EnumType.STRING)
-//     private Status status = Status.PENDING;
+    // Many bookings → One resource
+    @ManyToOne
+    @JoinColumn(name = "resource_id", nullable = false)
+    private Resource resource;
 
 
-//     // getters & setters
-// }
+    @CreationTimestamp
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    private String purpose;
+    private int attendees;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status = BookingStatus.PENDING;
+
+
+    // getters & setters
+}
